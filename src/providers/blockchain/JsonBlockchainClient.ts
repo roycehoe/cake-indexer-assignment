372e84f6ec25784b5aea4bd70fe25ec3293271bf
Eli <elicodesforfun@gmail.com>
2022-07-18 16:30:30 +0800

    feat: simulate non-deterministic response time

--- src/providers/blockchain/JsonBlockchainClient.ts
+++ src/providers/blockchain/JsonBlockchainClient.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import {Inject, Injectable, OnApplicationBootstrap, OnApplicationShutdown} from '@nestjs/common';
 import { readFileSync } from 'fs';
 import type { Block, BlockchainClient } from './_abstract';
 
@@ -7,12 +7,28 @@ export const JSON_BLOCKS = 'JSON_BLOCKS'
 /**
  * A blockchain client stub that holds a copy of the 'blockchain' as in-memory json objects
  * purely for the purposes of this technical assignment.
+ * It simulates non-deterministic response times to mimic real world behaviour.
  */
 @Injectable()
-export class JsonBlockchainClient implements BlockchainClient, OnApplicationBootstrap {
+export class JsonBlockchainClient implements BlockchainClient, OnApplicationBootstrap, OnApplicationShutdown {
   private blocks: Block[]
+  private timeouts: NodeJS.Timeout[] = [];
 
   constructor(@Inject(JSON_BLOCKS) private readonly path: string) {
+  }
+
+  async getBlockByHeight(height: number): Promise<Block | undefined> {
+    return this.simulateAsync(() => {
+      const blocks = this.blocks.filter(block => height === block.height);
+      return blocks[0];
+    });
+  }
+
+  async getBlockByHash(hash: string): Promise<Block | undefined> {
+    return this.simulateAsync(() => {
+      const blocks = this.blocks.filter(block => hash === block.hash);
+      return blocks[0];
+    })
   }
 
   /**
@@ -22,17 +38,27 @@ export class JsonBlockchainClient implements BlockchainClient, OnApplicationBoot
   onApplicationBootstrap(): void {
     this.blocks = JSON.parse(readFileSync(this.path).toString()) as Block[];
   }
-
   /**
-   * Fetches the block from a blockchain, simulating non-deterministic response time
+   * Called by Nest as part of app shutdown phase
+   * @see https://docs.nestjs.com/fundamentals/lifecycle-events
    */
-  async getBlockByHeight(height: number): Promise<Block | undefined> {
-    const blocks = this.blocks.filter(block => height === block.height);
-    return blocks[0];
+  onApplicationShutdown(): void {
+    this.timeouts.forEach(clearTimeout);
   }
 
-  async getBlockByHash(hash: string): Promise<Block | undefined> {
-    const blocks = this.blocks.filter(block => hash === block.hash);
-    return blocks[0];
+  private async simulateAsync<T>(callback: () => T): Promise<T> {
+    return new Promise<T>(resolve => {
+      // Simulate non-deterministic response time
+      const timeout = setTimeout(() => {
+        resolve(callback());
+      }, randIntInclusive(50, 500)); // ms
+
+      // Register timeout for clean-up
+      this.timeouts.push(timeout);
+    })
   }
+}
+
+function randIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
