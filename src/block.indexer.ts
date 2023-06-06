c8db36bc8bf058ee8e818a44481af123cb2cf319
roycehoe <royceinbox@hotmail.com>
2023-06-06 14:48:36 +0800

    feat: Create scaffolding of block height indexer

--- src/block.indexer.ts
+++ src/block.indexer.ts
@@ -7,8 +7,58 @@ import { BlockIndexerSearchField, BlockIndexerSearchParams } from './types';
  * TODO: Index the blocks provided by the client and expose via RESTful endpoint
  */
 
+function getBlockHashIndex(blocks: Block[]): Map<number, Block> {
+  const blockHashIndex = new Map();
+  for (const block of blocks) {
+    blockHashIndex.set(block.hash, block);
+  }
+  return blockHashIndex;
+}
+
+function getBlockHeightIndex(blocks: Block[]): Map<number, Block> {
+  const blockHeightIndex = new Map();
+  const suspiciousBlocks: Block[] = [];
+  let currentHeight = 0;
+
+  for (let i = 0; i < blocks.length - 1; i++) {
+    const currentBlock = blocks[i];
+    const nextBlock = blocks[i + 1];
+    if (
+      nextBlock.previousblockhash === currentBlock.hash &&
+      currentHeight !== nextBlock.height
+    ) {
+      //isLegitimateBlock
+      blockHeightIndex.set(currentBlock.height, currentBlock);
+      currentHeight = currentBlock.height;
+      continue;
+    }
+
+    if (currentBlock.height === nextBlock.height) {
+      suspiciousBlocks.push(currentBlock);
+      continue;
+    }
+
+    if (nextBlock.height !== currentBlock.height) {
+      blockHeightIndex.set(
+        currentBlock.height,
+        suspiciousBlocks.filter((block) => nextBlock.hash === block.hash)[0],
+      );
+    }
+  }
+
+  return blockHeightIndex;
+}
+
+function getTransactionAddressIndex(blocks: Block[]): Map<number, Block> {
+  return new Map();
+}
+
 @Injectable()
 export class BlockIndexer {
+  blockHashIndex: Map<number, Block> = new Map();
+  blockHeightIndex: Map<number, Block> = new Map();
+  transactionAddressIndex: Map<number, Block> = new Map();
+
   constructor(private readonly blockchainClient: JsonBlockchainClient) {}
 
   async getBlocks(height?: number): Promise<Block[]> {
--- src/providers/blockchain/_abstract.ts
+++ src/providers/blockchain/_abstract.ts
@@ -11,6 +11,72 @@ export interface BlockchainClient {
 
 export interface Block {
   hash: string;
+  confirmations: number;
+  strippedsize: number;
+  size: number;
+  weight: number;
   height: number;
-  // TODO: add more fields
+  masternode: string;
+  minter: string;
+  mintedBlocks: number;
+  stakeModifier: string;
+  version: number;
+  versionHex: string;
+  merkleroot: string;
+  nonutxo: Nonutxo[];
+  tx: Tx[];
+  time: number;
+  mediantime: number;
+  bits: string;
+  difficulty: number;
+  chainwork: string;
+  nTx: number;
+  previousblockhash: string;
+  nextblockhash: string;
+}
+
+export interface Nonutxo {
+  AnchorReward: number;
+  Burnt: number;
+}
+
+export interface Tx {
+  txid: string;
+  hash: string;
+  version: number;
+  size: number;
+  vsize: number;
+  weight: number;
+  locktime: number;
+  vin: Vin[];
+  vout: Vout[];
+  hex: string;
+}
+
+export interface Vin {
+  coinbase?: string;
+  sequence: number;
+  txid?: string;
+  vout?: number;
+  scriptSig?: ScriptSig;
+}
+
+export interface ScriptSig {
+  asm: string;
+  hex: string;
+}
+
+export interface Vout {
+  value: number;
+  n: number;
+  scriptPubKey: ScriptPubKey;
+  tokenId: number;
+}
+
+export interface ScriptPubKey {
+  asm: string;
+  hex: string;
+  reqSigs?: number;
+  type: string;
+  addresses?: string[];
 }
